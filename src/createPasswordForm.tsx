edc5924ef46682371d6c89d199e072108ce203d8
Tafeen <taafeenn@gmail.com>
2023-11-07 23:54:50 +0100

    Fixed linting

--- src/createPasswordForm.tsx
+++ src/createPasswordForm.tsx
@@ -21,27 +21,26 @@ export default function CreatePassForm() {
 
   const AddPASS = async (values: CreatingPassValues) => {
     let command = "";
-    
-    // Generate password without custom fields // tested
+
+    // Generate password without custom fields
     if (!values.password && !values.custom_fields) {
       command = `pass generate '${values.path}'`;
     }
 
-    // Generate password and use custom fields //tested
-    if (!values.password && values.custom_fields){
+    // Generate password and use custom fields
+    if (!values.password && values.custom_fields) {
       command = `export password=$(openssl rand -base64 12) && pass insert -m '${values.path}' << EOF\n$password\n${values.custom_fields}\nEOF`;
     }
 
-    // Use only password //tested
-    if (values.password && !values.custom_fields){
+    // Use only password
+    if (values.password && !values.custom_fields) {
       command = `echo '${values.password}' | pass insert -e '${values.path}'`;
     }
 
     // Use password and custom fields
-    if (values.password && values.custom_fields){
+    if (values.password && values.custom_fields) {
       command = `pass insert -m '${values.path}' << EOF\n${values.password}\n${values.custom_fields}\nEOF`;
     }
-
 
     const cmd = exec(command, options);
     const toast = await showToast({
@@ -77,11 +76,7 @@ export default function CreatePassForm() {
     >
       <Form.Description title="Create Password" text={"Create new password"} />
       <Form.TextField id="path" title="File path" />
-      <Form.PasswordField
-        id="password"
-        placeholder="If empty, password will be generated"
-        title="Password"
-      />
+      <Form.PasswordField id="password" placeholder="If empty, password will be generated" title="Password" />
       <Form.TextArea
         id="custom_fields"
         title="Custom Fields"
@@ -91,4 +86,3 @@ my_field:my_field_value"
     </Form>
   );
 }
-
